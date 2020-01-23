bf4e77eb066a1002fffca3dcb4c0d9fb04caea70
dorabby <dorabbi1010@gmail.com>
2020-01-23 10:41:34 +0900

    単体テストと画像投稿の修正

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -2,10 +2,11 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
-
   # Choose what kind of storage to use for this uploader:
   storage :file
   # storage :fog
+  process resize_to_fit: [400, 400]
+  #カリキュラムは400,400だった
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
@@ -44,5 +45,4 @@ class ImageUploader < CarrierWave::Uploader::Base
   # def filename
   #   "something.jpg" if original_filename
   # end
-  process resize_to_fit: [800, 800]
 end
