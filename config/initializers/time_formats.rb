7e84db889bb48ff60e6ca7c043eb5411d39c0724
dorabby <dorabbi1010@gmail.com>
2020-01-24 20:43:50 +0900

    日付データのメソッド変更2

--- config/initializers/time_formats.rb
+++ config/initializers/time_formats.rb
@@ -1,4 +1,1 @@
-Date::DATE_FORMATS[:default] = "%Y年%m月%d日"
-Date::DATE_FORMATS[:date] = "%m/%d"
-DateTime:DATE_FORMATS[:default] = "%Y年%m月%d日 %H時%M分"
-DateTime:DATE_FORMATS[:datetime] = "%Y/%m/%d %H:%M"
+Time::DATE_FORMATS[:default] = '%Y/%m/%d %H:%M'
